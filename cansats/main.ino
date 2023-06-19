07675eb9ea537bbf99ddc6bb7fbea26f07b1bfb4
ElenaSango <130225303+ElenaSango@users.noreply.github.com>
2023-06-19 12:47:10 +0200

    Update main.ino

--- cansats/main.ino
+++ cansats/main.ino
@@ -15,13 +15,18 @@
 */
 
 #include "Arduino_BMI270_BMM150.h"
+#include "Arduino_LPS22HB.h"
 
 float x, y, z;
 int degreesX = 0;
 int degreesY = 0;
 
 // Acceleration array
-int accArray[5] = {0, 0, 0, 0, 0};
+float accArray[] = {0, 0, 0, 0, 0};
+int idx = 0;
+
+int hasLiftedOff = 0;
+float initialPressure, currentPressure;
 
 void setup()
 {
@@ -36,10 +41,30 @@ void setup()
     while (1)
       ;
   }
+  
+  if (!BARO.begin()) {
+
+    Serial.println("Failed to initialize pressure sensor!");
+
+    while (1);
+
+  }
+
+  initialPressure = BARO.readPressure(MILLIBAR);
+  
 
   Serial.print("Accelerometer sample rate = ");
   Serial.print(IMU.accelerationSampleRate());
   Serial.println(" Hz");
+}
+
+int isFalling() {
+  for (int i = 0; i < 5; i++) {
+    if (accArray[i] > -0.3) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
 void loop()
@@ -50,43 +75,40 @@ void loop()
   {
     IMU.readAcceleration(x, y, z);
 
-    Serial.print("x: ");
+    /*Serial.print("x: ");
     Serial.print(x);
     Serial.print(" y: ");
     Serial.print(y);
     Serial.print(" z: ");
     Serial.print(z);
-    /*if (x > 0.1)
-    {
-      x = 100 * x;
-      degreesX = map(x, 0, 97, 0, 90);
-      Serial.print("Tilting up ");
-      Serial.print(degreesX);
-      Serial.println("  degrees");
+    Serial.println();*/
+    //Serial.println(z);
+    accArray[idx] = z;
+    if (idx == 5) {
+      idx = 0;
+    } else {
+      idx++;
     }
-    if (x < -0.1)
-    {
-      x = 100 * x;
-      degreesX = map(x, 0, -100, 0, 90);
-      Serial.print("Tilting down ");
-      Serial.print(degreesX);
-      Serial.println("  degrees");
+    if (isFalling()) {
+      //Serial.println("Cayendo!!");
+    }else {
+      //Serial.println("Subiendo!!");
     }
-    if (y > 0.1)
-    {
-      y = 100 * y;
-      degreesY = map(y, 0, 97, 0, 90);
-      Serial.print("Tilting left ");
-      Serial.print(degreesY);
-      Serial.println("  degrees");
-    }
-    if (y < -0.1)
-    {
-      y = 100 * y;
-      degreesY = map(y, 0, -100, 0, 90);
-      Serial.print("Tilting right ");
-      Serial.print(degreesY);
-      Serial.println("  degrees");
-    }*/
   }
+
+  currentPressure = BARO.readPressure(MILLIBAR);
+  if (!hasLiftedOff && (initialPressure - currentPressure) > 1) {
+    hasLiftedOff = 1;
+  }
+
+  if (hasLiftedOff) {
+    Serial.println("Estamos volando!!");
+    } else {
+      Serial.println("No hemos despegado aun");}
+
+
+  // print an empty line
+
+  Serial.println();
+  delay(1000);
 }
