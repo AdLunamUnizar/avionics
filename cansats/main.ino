c734444a57e3f4cc95a6ffb965fbb67df022ce60
ayubero <32571894+ayubero@users.noreply.github.com>
2023-06-19 11:14:58 +0200

    Update main.ino

--- cansats/main.ino
+++ cansats/main.ino
@@ -20,14 +20,21 @@ float x, y, z;
 int degreesX = 0;
 int degreesY = 0;
 
-void setup() {
+// Acceleration array
+int accArray[5] = {0, 0, 0, 0, 0};
+
+void setup()
+{
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println("Started");
 
-  if (!IMU.begin()) {
+  if (!IMU.begin())
+  {
     Serial.println("Failed to initialize IMU!");
-    while (1);
+    while (1)
+      ;
   }
 
   Serial.print("Accelerometer sample rate = ");
@@ -35,39 +42,51 @@ void setup() {
   Serial.println(" Hz");
 }
 
-void loop() {
+void loop()
+{
   float x, y, z;
 
-  if (IMU.accelerationAvailable()) {
+  if (IMU.accelerationAvailable())
+  {
     IMU.readAcceleration(x, y, z);
 
-if(x > 0.1){
-    x = 100*x;
-    degreesX = map(x, 0, 97, 0, 90);
-    Serial.print("Tilting up ");
-    Serial.print(degreesX);
-    Serial.println("  degrees");
+    Serial.print("x: ");
+    Serial.print(x);
+    Serial.print(" y: ");
+    Serial.print(y);
+    Serial.print(" z: ");
+    Serial.print(z);
+    /*if (x > 0.1)
+    {
+      x = 100 * x;
+      degreesX = map(x, 0, 97, 0, 90);
+      Serial.print("Tilting up ");
+      Serial.print(degreesX);
+      Serial.println("  degrees");
     }
-  if(x < -0.1){
-    x = 100*x;
-    degreesX = map(x, 0, -100, 0, 90);
-    Serial.print("Tilting down ");
-    Serial.print(degreesX);
-    Serial.println("  degrees");
+    if (x < -0.1)
+    {
+      x = 100 * x;
+      degreesX = map(x, 0, -100, 0, 90);
+      Serial.print("Tilting down ");
+      Serial.print(degreesX);
+      Serial.println("  degrees");
     }
-  if(y > 0.1){
-    y = 100*y;
-    degreesY = map(y, 0, 97, 0, 90);
-    Serial.print("Tilting left ");
-    Serial.print(degreesY);
-    Serial.println("  degrees");
+    if (y > 0.1)
+    {
+      y = 100 * y;
+      degreesY = map(y, 0, 97, 0, 90);
+      Serial.print("Tilting left ");
+      Serial.print(degreesY);
+      Serial.println("  degrees");
     }
-  if(y < -0.1){
-    y = 100*y;
-    degreesY = map(y, 0, -100, 0, 90);
-    Serial.print("Tilting right ");
-    Serial.print(degreesY);
-    Serial.println("  degrees");
-    }
+    if (y < -0.1)
+    {
+      y = 100 * y;
+      degreesY = map(y, 0, -100, 0, 90);
+      Serial.print("Tilting right ");
+      Serial.print(degreesY);
+      Serial.println("  degrees");
+    }*/
   }
 }
