dde85b20b58004ee81db825871845d7683e2e665
berrujaime <berrujaime@gmail.com>
2023-09-19 19:35:41 +0200

    LORA serial, calibrate baro, HIGH buzzer and parachute

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,10 @@
 #include "Arduino_BMI270_BMM150.h"
 #include "Arduino_LPS22HB.h"
+#include <SoftwareSerial.h>
 
-#define minAltitudeToDeployParachute 1 // 20 meters minimum to deploy parachute
+#define minAltitudeToDeployParachute 20 // 20 meters minimum to deploy parachute
+#define parachutePin 4
+#define alertPin 5
 
 // GLOBAL VARIABLES ------------------------------------------------------------
 // Acceleration measurements
@@ -18,15 +21,33 @@ int monotonousChanges = 0;
 bool hasLiftedOff = false;
 // END GLOBAL VARIABLES --------------------------------------------------------
 
+// Serial LORA port
+SoftwareSerial mySerial(2, 3);
+
+// TODO: comprobar que el serial está disponible es necesario?
+
 // Function to setup the serial port, IMU and barometer
 void setup()
 {
+  digitalWrite(parachutePin, LOW);
+  digitalWrite(alertPin, LOW);
+  // Configurate pins 4 and 5 as output
+  pinMode(parachutePin, OUTPUT);
+  pinMode(alertPin, OUTPUT);
+  
   // Inicialization of serial port
   Serial.begin(9600);
   // Comment this loop when testing without computer
   while (!Serial)
     ;
   Serial.println("Started");
+
+  // Inicialization of LORA serial port
+  mySerial.begin(9600);
+  // Comment this loop when testing without computer
+  while (!mySerial.available())
+    ;
+  mySerial.println("Started LORA serial port");
 
   // Uncomment this code if you want to use the IMU
   /*
@@ -46,6 +67,7 @@ void setup()
   if (!BARO.begin())
   {
     Serial.println("Failed to initialize pressure sensor!");
+    mySerial.println("Failed to initialize pressure sensor!");
     while (1)
       ;
   }
@@ -56,25 +78,29 @@ void setup()
   bool calibrated = false;
 
   // Loop until the barometer is calibrated
-  /*while(!calibrated){
+  while(!calibrated){
     Serial.println("Calibrating barometer...");
-    String msg = Serial.readString();
-    // String msg = "Initial _Altitude:X.XX";
-    if (msg.startsWith("Initial _Altitude:"))
+    mySerial.println("Calibrating barometer...");
+    String msg = mySerial.readString();
+    // String msg = "Initial_Altitude:X.XX";
+    if (msg.startsWith("Initial_Altitude:"))
     {
       initialAltitude = msg.substring(18).toFloat();
       Serial.print("Initial altitude: ");
+      mySerial.print("Initial altitude: ");
       Serial.println(initialAltitude);
+      mySerial.println(initialAltitude);
       // Exit loop
       calibrated = true;
     }
     else
     {
       Serial.println("Bad command - Initial altitude not set");
+      mySerial.println("Bad command - Initial altitude not set");
     }
-  }*/
+  }
 
-  initialAltitude = 180.0f; // Remove
+  //initialAltitude = 180.0f; // Remove
   initialPressure = BARO.readPressure();
 }
 
@@ -88,17 +114,21 @@ bool isFalling()
     // The rocket is descending
 
     Serial.println("Se detecta cambio de monotonía");
+    mySerial.println("Se detecta cambio de monotonía");
 
     monotonousChanges++;
     
     Serial.print("Cambios de monotonía: ");
     Serial.println(monotonousChanges);
+    mySerial.print("Cambios de monotonía: ");
+    mySerial.println(monotonousChanges);
 
     if (monotonousChanges == 3 && hasLiftedOff) {
       /*At least 3 consecutive changes in monotonicity have occurred, the rocket
        *has reached its apogee and started descending.
        */
       Serial.println("Cohete descendiendo");
+      mySerial.println("Cohete descendiendo");
       return true;
     }
   }
@@ -119,13 +149,21 @@ void checkDeployParachute()
   if (isFalling() && hasLiftedOff)
   {
     Serial.println("Desplegamos paracaídas!!");
-    // TODO: Poner pin a HIGH para desplegar paracaídas
+    // Activate alert sound 
+    digitalWrite(alertPin, HIGH);
+    delay(3000);
+    // Deactivate alert sound 
+    digitalWrite(alertPin, LOW);
+    
+    // Deploy parachute
+    digitalWrite(parachutePin, HIGH);
 
     continueTelemetry();
   }
   else
   {
     Serial.println("Subiendo o no hemos despegado");
+    mySerial.println("Subiendo o no hemos despegado");
   }
 }
 
@@ -136,6 +174,8 @@ void checkAltitude()
   float currentPressure = BARO.readPressure();
   Serial.print("Presión (kPa): ");
   Serial.println(currentPressure);
+  mySerial.print("Presión (kPa): ");
+  mySerial.println(currentPressure);
 
   // Update last altitude
   lastAltitude = currentAltitude;
@@ -144,6 +184,8 @@ void checkAltitude()
   currentAltitude = 44330 * ( 1 - pow(currentPressure/initialPressure, 1/5.255) );
   Serial.print("Altitud relativa (m): ");
   Serial.println(currentAltitude);
+  mySerial.print("Altitud relativa (m): ");
+  mySerial.println(currentAltitude);
 
   // Check if the rocket has lifted off
   if(!hasLiftedOff && (currentAltitude > minAltitudeToDeployParachute)) {
